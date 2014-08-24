58a0ae64bc57090e0d855b2f7daf4ae5970ffe25
Alekseylv <alekseylv@me.com>
2014-08-24 13:57:23 +0300

    error fix

--- js/lv.js
+++ js/lv.js
@@ -1,6 +1,6 @@
 var lv_lang = {
     whatWeDoTitle: "Par mums",
-    whatWeDoText1:"Mūsu organizācija ir dibināta 2005. gada aprīla mēnesī, kā ģimenes uzņēmums, šos gadus atsauksmes par mums ir bijušas labas un cenšamies pēc iespējas ieviest savā darbā jaunievedumus un paplašināt darba iespējas un darbības sfēras. Uzlabot tehniku un veidot savu darbu pieejamāku un izdevīgāku priekš pasūtītājiem.",
+    whatWeDoText1: "Mūsu organizācija ir dibināta 2005. gada aprīla mēnesī, kā ģimenes uzņēmums, šos gadus atsauksmes par mums ir bijušas labas un cenšamies pēc iespējas ieviest savā darbā jaunievedumus un paplašināt darba iespējas un darbības sfēras. Uzlabot tehniku un veidot savu darbu pieejamāku un izdevīgāku pasūtītājiem.",
     whatWeDoText2: "Mūsu darba sfēra šobrīt ir celtniecības atkritumu izvešana, to savākšana ar iekrāvējiem, dažādu tilpumu konteineru noma, BoBcata noma, kā ārī veicam darbus ar teleskopiskā iekrāvēja palīdzību.",
     contact: "Kontakti",
     order: "Тālrunis pasūtijumiem",
