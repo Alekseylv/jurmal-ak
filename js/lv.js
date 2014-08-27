78f297682f8a742c8eb1e4419e1597a8b3c8b56d
Alekseylv <alekseylv@me.com>
2014-08-27 21:10:08 +0300

    changed lv text

--- js/lv.js
+++ js/lv.js
@@ -1,7 +1,7 @@
 var lv_lang = {
     whatWeDoTitle: "Par mums",
-    whatWeDoText1: "Mūsu organizācija ir dibināta 2005. gada aprīla mēnesī, kā ģimenes uzņēmums, šos gadus atsauksmes par mums ir bijušas labas un cenšamies pēc iespējas ieviest savā darbā jaunievedumus un paplašināt darba iespējas un darbības sfēras. Uzlabot tehniku un veidot savu darbu pieejamāku un izdevīgāku pasūtītājiem.",
-    whatWeDoText2: "Mūsu darba sfēra šobrīt ir celtniecības atkritumu izvešana, to savākšana ar iekrāvējiem, dažādu tilpumu konteineru noma, BoBcata noma, kā ārī veicam darbus ar teleskopiskā iekrāvēja palīdzību.",
+    whatWeDoText1: "Mūsu organizācija ir dibināta 2005. gada aprīļa mēnesī, kā ģimenes uzņēmums. Šos gadus atsauksmes par mums ir bijušas labas un cenšamies pēc iespējas ieviest savā darbā jaunievedumus un paplašināt darba iespējas un darbības sfēras, uzlabot tehniku un veidot savu darbu pieejamāku un izdevīgāku pasūtītājiem.",
+    whatWeDoText2: "Mūsu darba sfēra šobrīd ir celtniecības atkritumu izvešana, to savākšana ar iekrāvējiem, dažādu tilpumu konteineru noma, kā arī veicam darbus ar teleskopiskā iekrāvēja palīdzību.",
     contact: "Kontakti",
     order: "Тālrunis pasūtijumiem",
     agreement: "Tālrunis līgumu slēgšanai",
